3d7bd4e0cb91e87b96e616b60b29f71831803177
jwinter2011 <jeremy.winter@selectcomfort.com>
2017-03-17 15:00:10 -0500

    added navbar-collapse

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 //will prevent unexpected menu close when using some components (like accordion, forms, etc)
 {$(function() {
 	window.prettyPrint && prettyPrint()
-	$(document).on('click', '.yamm .dropdown-menu', function(e) {
+	$(document).on('click', '.dropdown-menu', function(e) {
 		e.stopPropagation()
 })
 })};
@@ -11,3 +11,16 @@
 function myFunction(x) {
 	x.classList.toggle("change");
 };
+
+
+/*
+bootstrap menu click 
+collapse bootstrap navbar on click in responsive view. on same page click.
+*/
+$(function(){ 
+     var navMain = $(".navbar-collapse");
+     
+     navMain.on("click", "a", null, function () {
+         navMain.collapse('hide');
+     });
+ });
