a1145dbb73258f124b0758c7ee69bee9c73458bd
jwinter2011 <jeremy.winter@selectcomfort.com>
2017-03-20 11:53:11 -0500

    Tidied up style sheet

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-//will prevent unexpected menu close when using some components (like accordion, forms, etc)
+// will prevent unexpected menu close when using some components (like accordion, forms, etc)
 $(function() {
 	window.prettyPrint && prettyPrint()
-	$(document).on('click', '.yamm .dropdown-menu', function(e) {
-	  e.stopPropagation()
-	})
+		$(document).on('click', '.yamm .dropdown-menu', function(e) {
+			e.stopPropagation()
+		})
 })
 
 // nav bar animation
